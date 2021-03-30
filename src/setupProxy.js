283f0d893f19e43163d52ac25c2f96f1aa632a97
Hwang In Seo <sjsjsj1246@gmail.com>
2021-03-31 06:42:44 +0900

    init

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/data",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "https://talk-about-server.herokuapp.com/",
       changeOrigin: true,
     })
   );
